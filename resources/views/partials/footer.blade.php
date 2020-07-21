4b1ed96dded3d1eab2362678553ace1a0ccf0f8d
theprophecy <thearyanahmed@gmail.com>
2020-07-21 23:14:43 +0600

    added favicons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="flow-root mb-20">
+<div class="flow-root mx-10">
     <div class="my-4 block text-sfh text-center px-4 py-2">
-        copyright Aryan's
+        aryan's
     </div>
 </div>
